84d75e5e6533c4b6ad8c6e39c4c42eb56e28386a
GreenRats2051 <143582652+GreenRats2051@users.noreply.github.com>
2024-09-05 10:37:15 +0300

    PlayerController
    -Add PlayerController
    -Add DeveloperLevel

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera MainCamera; //Камера игрока
+    public LayerMask WeaponMask; //Маска оружия
+    public Transform PlayerModel; //Модель игрока
+    public Vector3 Point; //Точка мыши
+    private Vector2 InputAction; //Нажатие вверх, вниз, влево, впрао
+    private Collider[] Weapons; //Ближайшее оружие
+    public Rigidbody Rigidbody; //Rigidbody игрока
+    public Weapon[] PlayerWeapons; //Оружие игрока
+    public float Speed; //Скорость игрока
+    public float RadiusCheckWeapon; //Радиус поднятия оружия
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Movement(); //Передвижение
+        WeaponPickUp(); //Поднятие оружия
+    }
+
+    void Movement()
+    {
+        InputAction.x = Input.GetAxis("Horizontal");
+        InputAction.y = Input.GetAxis("Vertical");
+        Rigidbody.velocity = new Vector3(InputAction.x * Speed, Rigidbody.velocity.y, InputAction.y * Speed);
+        RaycastHit RaycastHit;
+        Ray RayMainCamera = MainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(RayMainCamera, out RaycastHit, Mathf.Infinity))
+        {
+            Point = new Vector3(RaycastHit.point.x, 1, RaycastHit.point.z);
+        }
+        PlayerModel.LookAt(new Vector3(Point.x, transform.position.y, Point.z));
+    }
+
+    void WeaponPickUp()
+    {
+        Weapons = Physics.OverlapSphere(transform.position, RadiusCheckWeapon, WeaponMask);
+        if (Weapons.Length != 0)
+        {
+            for (int i = 0; i < PlayerWeapons.Length; i++)
+            {
+                if (Weapons[0].name == PlayerWeapons[i].PlayerWeaponModel.name && PlayerWeapons[i].IsHavyPlayer == false)
+                {
+                    PlayerWeapons[i].IsHavyPlayer = true;
+                    PlayerWeapons[i].PlayerWeaponModel.SetActive(true);
+                    Debug.Log("Оружие " + PlayerWeapons[i].PlayerWeaponModel.name + " теперь " + PlayerWeapons[i].IsHavyPlayer);
+                    Destroy(Weapons[0]);
+                }
+                else if (Weapons[0].name == PlayerWeapons[i].PlayerWeaponModel.name && PlayerWeapons[i].IsHavyPlayer == true)
+                {
+                    Debug.Log("Добавлены патроны к " + PlayerWeapons[i].PlayerWeaponModel.name);
+                }
+                else
+                {
+                    PlayerWeapons[i].PlayerWeaponModel.SetActive(false);
+                }
+            }
+        }
+    }
+}
+
+[Serializable]
+public class Weapon
+{
+    public GameObject PlayerWeaponModel;
+    public bool IsHavyPlayer;
+}
